912ff3ec9d86946adf0ff82ccff5d415cd7a6989
Vladimir-Kharchenko3 <141916443+Vladimir-Kharchenko3@users.noreply.github.com>
2023-12-14 17:23:29 +0300

    Решил задачу 3

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int arraySize = ReadInt("Введите размер массива: ");
+
+        int[] array = new int[arraySize];
+
+        for (int i = 0; i < arraySize; i++)
+        {
+            array[i] = i + 1;
+        }
+
+        PrintArrayReverse(array, array.Length - 1);
+    }
+
+    static int ReadInt(string str)
+    {
+        System.Console.Write(str);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+
+        if (index < 0)
+            return;
+
+        Console.Write(arr[index] + " ");
+
+        PrintArrayReverse(arr, index - 1);
+    }
+}
