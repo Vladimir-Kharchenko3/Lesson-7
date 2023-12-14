934be158cd75bfb7e8b61a525870c1ead8351e9f
Vladimir-Kharchenko3 <141916443+Vladimir-Kharchenko3@users.noreply.github.com>
2023-12-14 17:13:48 +0300

    Решил задачу 1

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        
+        int M = ReadInt("Введите начальное значение (M): ");
+        int N = ReadInt("Введите конечное значение (N): ");
+
+        PrintNumbersInRange(M, N);
+    }
+
+    static int ReadInt(string str)
+    {
+        System.Console.Write(str);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static void PrintNumbersInRange(int start, int end)
+    {
+        
+        if (start > end)
+            return;
+
+        Console.Write(start + " ");
+        PrintNumbersInRange(start + 1, end);
+    }
+}
